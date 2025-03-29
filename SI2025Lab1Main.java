8fff9f8228d08745809286c7a464032bf6ee3abd
Gjoshe Todorov <todorovgjose@gmail.com>
2025-03-29 19:35:49 +0100

    Implement markCategoryCompleted method

--- SI2025Lab1Main.java
+++ SI2025Lab1Main.java
@@ -107,10 +107,16 @@ class TaskManager {
     }
 
     // 9. Mark all tasks in a category as completed
-    public void markCategoryCompleted(String category) {
-        // TODO: Implement bulk completion logic
-    }
-}
+
+	public void markCategoryCompleted(String category) {
+    for (Task task : tasks) {
+        if (task.getCategory().equalsIgnoreCase(category)) {
+            task.setCompleted(true); // Mark task as completed
+        		}
+    		}
+	}
+
+   }
 
 public class SI2025Lab1Main {
     public static void main(String[] args) {
